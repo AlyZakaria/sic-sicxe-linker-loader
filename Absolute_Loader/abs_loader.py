44420ccb84f196dd57f494ac49b85ef4116c1943
AlyZakaria <77212759+AlyZakaria@users.noreply.github.com>
2022-12-07 11:45:10 +0200

    Absolute_loader refactoring

--- Absolute_Loader/abs_loader.py
+++ Absolute_Loader/abs_loader.py
@@ -1,60 +1,45 @@
-from main import *
 import re
+from Absolute_Loader import helper_function as hf
 
 
-# get which row in memory graph
-def get_row(start_record, start_program):
-    row = int(start_record, base=16) - int(start_program, base=16)
-    row = (row / 16)
-    return int(row)
 
+# get The Absolute loader
+def get_absolute_loader(df):
+    # open sic input file
+    f = open('rsc\\sic.txt', 'r')
 
-def get_column_index(column_name):
-    column_index = df.columns.get_loc(column_name)
-    return column_index
+    # get the starting address & program length
+    header_string = f.readline()
+    starting_address = hf.hex_to_string(header_string[7:13])
+    program_length = hf.hex_to_string(header_string[14:])
+    
+    # calculate addresses of memory
+    addresses = []
+    df = hf.getMemoryGraph(starting_address, program_length, addresses, df)
 
-
-# open sic input file
-f = open('rsc\\sic.txt', 'r')
-header_string = f.readline()
-starting_address = hex(int(header_string[7:13], base=16))[2:].zfill(4).upper()
-program_length = hex(int(header_string[14:], base=16))[2:].zfill(4).upper()
-print(program_length)
-if starting_address[3] != '0':
-    starting_address = starting_address[:3] + '0'
-# calculate addresses of memory
-addresses = []
-for i in range(int(starting_address, base=16), int(program_length, base=16)+int(starting_address, base=16), 16):
-    addresses.append(hex(i)[2:].zfill(4))
-df['Address'] = addresses
-df = df.fillna('xx')
-
-# insert into cells
-for line in f.readlines():
-    if line[0] == 'T':
-        cells_no = len(line[9:]) / 2
-        record_length = line[7:9]
-        record_start = hex(int(line[1:7], base=16))[2:].zfill(4).upper()
-
-        # slice record into tuples
-        obj_codes = line[9:]
-        tuples = re.findall('..', obj_codes)
-        tuples_counter = 0
-
-        which_column = record_start[3]
-        which_column = get_column_index(which_column)
-        which_row = get_row(record_start, addresses[0])
-        flag = False
-        for row in range(which_row, which_row + 3, 1):
-            for column in range(which_column, 17):
-                if tuples_counter < len(tuples):
-                    df.iloc[row, column] = tuples[tuples_counter]
-                    tuples_counter += 1
-                else:
-                    flag = True
+    # insert into cells
+    for line in f.readlines():
+        if line[0] == 'T':
+            record_start = hf.hex_to_string(line[1:7])
+            # slice record into tuples
+            obj_codes = line[9:]
+            tuples = re.findall('..', obj_codes)
+            tuples_counter = 0
+            # get the index of starting cell
+            [which_row, which_column] = hf.get_cell(record_start, addresses[0], record_start[3], df)
+            flag = False
+            for row in range(which_row, which_row + 3, 1):
+                for column in range(which_column, 17):
+                    if tuples_counter < len(tuples):
+                        df.iloc[row, column] = tuples[tuples_counter]
+                        tuples_counter += 1
+                    # if we exceed the size of tuples(records) break
+                    else:
+                        flag = True
+                        break;
+                which_column = 1
+                if flag:
                     break;
-            which_column = 1
-            if flag:
-                break;
+    return df
 
-print(df)
+
--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import pandas as pd
 from Absolute_Loader import abs_loader
-df = pd.read_csv("dataFrame.txt", sep=' ')
+
+list = ['Address', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ,
+ 'A' , 'B' , 'C' , 'D' , 'E' , 'F' ]
+
+df = pd.DataFrame(columns = list)
 
 # Absolute loader
+print(abs_loader)
+df = abs_loader.get_absolute_loader(df)
+print(df)
